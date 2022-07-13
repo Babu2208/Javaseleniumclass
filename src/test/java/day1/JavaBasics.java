d9cd84c4620c1d32812e22d2f8d6d996e6e10f31
Babu2208 <seluvababu@gmail.com>
2022-07-13 13:55:38 +0530

    Adding new file

---
+++ src/test/java/day1/JavaBasics.java
@@ -0,0 +1,29 @@
+package day1;
+
+public class JavaBasics {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 20;
+
+        int sub = y-x;
+        int sum = x+y;
+        int mul = x*y;
+        int div = y/x;
+        int mod = y%x;
+        x++;
+        y--;
+
+
+            System.out.println("Sub "+ sub);
+            System.out.println("Sum "+ sum);
+            System.out.println("Mul "+ mul);
+            System.out.println("Div "+ div);
+            System.out.println("Mod "+mod);
+            System.out.println("x ="+ x);
+            System.out.println("y ="+y);
+
+
+    }
+}
