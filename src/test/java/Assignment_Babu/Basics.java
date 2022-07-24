919ef6c2f226f8319cbdf08cca1c08b767727d9d
Babu2208 <seluvababu@gmail.com>
2022-07-24 09:53:49 +0530

    Practice

--- src/test/java/Assignment_Babu/Basics.java
+++ src/test/java/Assignment_Babu/Basics.java
@@ -1,4 +1,4 @@
-package day1;
+package Assignment_Babu;
 
 public class Basics {
     public static void main(String[] args) {
--- src/test/java/Assignment_Babu/Operators.java
+++ src/test/java/Assignment_Babu/Operators.java
@@ -1,4 +1,4 @@
-package day1;
+package Assignment_Babu;
 
 public class Operators {
 
